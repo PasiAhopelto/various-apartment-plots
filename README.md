0f5b1314ec8bc5baca226c3cf44e602c6b42f289
Pasi Ahopelto <pahopelt@gmail.com>
2015-02-03 22:06:04 +0200

    Initial commit of first plot

---
+++ README.md
@@ -0,0 +1,4 @@
+various-apartment-plots
+=======================
+
+Toying with R by making various plots of data available from Tilastokeskus (http://tilastokeskus.fi).
---
+++ vanhojen-vapaarahoitteisten-asuntojen-hinnat-postinumeroalueittain-ja-rakennusvuosittain.R
@@ -0,0 +1,10 @@
+library(pxR)
+
+# Commented out download for preventing repeated downloads during development, uncomment if running first time
+# download.file(url = "http://193.166.171.75/Database/StatFin/Asu/ashi/004_ashi_tau_109.px", destfile = "vanhat-vuosittain.px")
+
+# Commented out to speed up development, uncomment if running for first time
+# data <- read.px(filename = "vanhat-vuosittain.px")
+
+plotByPostalCode <- function(postalCode) {
+}
